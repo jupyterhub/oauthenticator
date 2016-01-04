d2fe245151954cba99143ae0a3665e4ecb85006b
Min RK <benjaminrk@gmail.com>
2016-01-04 12:05:00 +0100

    release 0.2.0
    with MediaWiki

--- oauthenticator/_version.py
+++ oauthenticator/_version.py
@@ -5,11 +5,11 @@
 
 version_info = (
     0,
+    2,
     0,
-    1,
-    'dev', # comment-out this line for a release
+    # 'dev', # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
 if len(version_info) > 3:
-    __version__ = '%s-%s' % (__version__, version_info[3])
+    __version__ = '%s%s' % (__version__, version_info[3])
