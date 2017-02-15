ed891dbb81e1a313ff814152df2ccb57bb55735d
Min RK <benjaminrk@gmail.com>
2017-02-15 16:57:20 +0100

    consolidate Google OAuth settings into Authenticator.authenticate
    makes it more similar to others and easier to test

--- oauthenticator/google.py
+++ oauthenticator/google.py
@@ -31,28 +31,10 @@ class GoogleLoginHandler(OAuthLoginHandler, GoogleOAuth2Mixin):
             scope=['openid', 'email'],
             response_type='code')
 
-class GoogleOAuthHandler(OAuthCallbackHandler, GoogleOAuth2Mixin):
-    @gen.coroutine
-    def get(self):
-        self.settings['google_oauth'] = {
-            'key': self.authenticator.client_id,
-            'secret': self.authenticator.client_secret,
-            'scope': ['openid', 'email']
-        }
-        self.log.debug('google: settings: "%s"', str(self.settings['google_oauth']))
-        # FIXME: we should verify self.settings['google_oauth']['hd']
 
-        # "Cannot redirect after headers have been written" ?
-        #OAuthCallbackHandler.get(self)
-        username = yield self.authenticator.get_authenticated_user(self, None)
-        self.log.info('google: username: "%s"', username)
-        if username:
-            user = self.user_from_username(username)
-            self.set_login_cookie(user)
-            self.redirect(url_path_join(self.hub.server.base_url, 'home'))
-        else:
-            # todo: custom error
-            raise HTTPError(403)
+class GoogleOAuthHandler(OAuthCallbackHandler, GoogleOAuth2Mixin):
+    pass
+
 
 class GoogleOAuthenticator(OAuthenticator, GoogleOAuth2Mixin):
 
@@ -75,6 +57,11 @@ class GoogleOAuthenticator(OAuthenticator, GoogleOAuth2Mixin):
         code = handler.get_argument('code', False)
         if not code:
             raise HTTPError(400, "oauth callback made without a token")
+        handler.settings['google_oauth'] = {
+            'key': self.client_id,
+            'secret': self.client_secret,
+            'scope': ['openid', 'email']
+        }
         user = yield handler.get_authenticated_user(
             redirect_uri=self.get_callback_url(handler),
             code=code)
