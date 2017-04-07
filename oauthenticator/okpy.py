83abe569d1bf5efd2337a64ffd9109be83717007
Steven Wu <stevenwuyinze@berkeley.edu>
2017-04-07 13:09:14 -0700

    Finalized OkpyOAuthenticator. Refactored. Token now saved to db and can be transferred to env var.

--- oauthenticator/okpy.py
+++ oauthenticator/okpy.py
@@ -24,22 +24,47 @@ OKPY_USER_URL = "https://okpy.org/api/v3/user"
 OAUTH_ACCESS_TOKEN_URL = "https://okpy.org/oauth/token"
 OAUTH_AUTHORIZE_URL =  "https://okpy.org/oauth/authorize"
 
-class OkpyMixin(OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = OAUTH_AUTHORIZE_URL
-    _OAUTH_ACCESS_TOKEN_URL = OAUTH_ACCESS_TOKEN_URL
-
-class OkpyLoginHandler(OAuthLoginHandler, OkpyMixin):
-    pass
-
-class OkpyCallbackHandler(BaseHandler):
-    """Basic handler for Okpy callback.
-    Calls authenticator to verify username. Then it sets the state in the env var.
+class OkpyLoginHandler(OAuthLoginHandler, OAuth2Mixin):
+    """ An OAuthLoginHandler that provides scope to
+        OkpyMixin's authorize_redirect.
     """
+    _OAUTH_ACCESS_TOKEN_URL = "https://okpy.org/oauth/token"
+    _OAUTH_AUTHORIZE_URL =  "https://okpy.org/oauth/authorize"
+    def get(self):
+        self.authorize_redirect(
+            redirect_uri = self.authenticator.oauth_callback_url,
+            client_id = self.authenticator.client_id,
+            scope= ['all'],
+            response_type = 'code')
+
+class OkpyCallbackHandler(BaseHandler, OAuth2Mixin):
+    """ Basic handler for Okpy callback.
+        Calls authenticator to verify user and saves the state in user.
+    """
+    def get_auth_request(self, code):
+        params = dict(
+            redirect_uri = self.authenticator.oauth_callback_url,
+            code = code,
+            grant_type = 'authorization_code'
+        )
+        b64key = base64.b64encode(
+            bytes("{}:{}".format(self.authenticator.client_id,
+                                 self.authenticator.client_secret), "utf8"
+                 )
+        )
+        url = url_concat(OAUTH_ACCESS_TOKEN_URL, params)
+        req = HTTPRequest(url,
+                method = "POST",
+                headers = { "Accept": "application/json",
+                            "User-Agent": "JupyterHub",
+                            "Authorization": "Basic {}".format(b64key.decode("utf8"))},
+                            body = '' # Body is required for a POST...
+                          )
+        return req
+
     @gen.coroutine
     def get(self):
-        # TODO: Check if state argument needs to be checked
         username, state = yield self.authenticator.authenticate(self, None)
-
         if username:
             user = self.user_from_username(username)
             user.auth_state = state
@@ -48,51 +73,39 @@ class OkpyCallbackHandler(BaseHandler):
             self.set_login_cookie(user)
             self.redirect(url_path_join(self.hub.server.base_url, 'home'))
         else:
-            # todo: custom error page?
-            raise web.HTTPError(403)
+            raise web.HTTPError(403, "Authentication Failed.")
 
-class OkpyOAuthenticator(OAuthenticator):
-    login_service = "okpy"
-    callback_handler = OkpyCallbackHandler
-    login_handler = OkpyLoginHandler
-    @gen.coroutine
-    def authenticate(self, handler, data=None):
-        code = handler.get_argument("code", False)
-        if not code:
-            raise web.HTTPError(400, "Authentication Failed.")
-        http_client = AsyncHTTPClient()
-        params = dict(
-            redirect_uri = "http://localhost:8000/hub/oauth_callback",
-            code=code,
-            grant_type='authorization_code'
-        )
-        b64key = base64.b64encode(
-            bytes(
-                "{}:{}".format(self.client_id, self.client_secret),
-                "utf8"
-            )
-        )
-        url = url_concat(OAUTH_ACCESS_TOKEN_URL, params)
-        req = HTTPRequest(url,
-                          method = "POST",
-                          headers = {"Accept": "application/json",
-                                     "User-Agent": "JupyterHub",
-                                     "Authorization": "Basic {}".format(b64key.decode("utf8"))},
-                          body = '' # Body is required for a POST...
-                          )
-        resp = yield http_client.fetch(req)
-        state = json.loads(resp.body.decode('utf8', 'replace'))
-        access_token = state['access_token']
-        # Determine who the logged in user is
+    def get_user_info_request(self, access_token):
         headers = {"Accept": "application/json",
                    "User-Agent": "JupyterHub",
                    "Authorization": "Bearer {}".format(access_token)}
-        url = url_concat(OKPY_USER_URL, {"envelope" : "false"})
-        req = HTTPRequest(url, method="GET", headers=headers)
-        resp = yield http_client.fetch(req)
-        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-        print(resp_json)
-        return resp_json["email"], state
+        params = {"envelope" : "false"}
+        url = url_concat(OKPY_USER_URL, params)
+        req = HTTPRequest(url, method = "GET", headers = headers)
+        return req
+
+class OkpyOAuthenticator(OAuthenticator, OAuth2Mixin):
+    login_service = "Okpy"
+    callback_handler = OkpyCallbackHandler
+    login_handler = OkpyLoginHandler
+    @gen.coroutine
+    def authenticate(self, handler, data = None):
+        code = handler.get_argument("code", False)
+        if not code:
+            raise web.HTTPError(400, "Authentication Cancelled.")
+        print(handler)
+        http_client = AsyncHTTPClient()
+        auth_request = handler.get_auth_request(code)
+        response = yield http_client.fetch(auth_request)
+        if not response:
+            self.clear_all_cookies()
+            raise HTTPError(500, 'Authentication Failed: Token Not Acquired')
+        state = json.loads(response.body.decode('utf8', 'replace'))
+        access_token = state['access_token']
+        info_request = handler.get_user_info_request(access_token)
+        response = yield http_client.fetch(info_request)
+        user = json.loads(response.body.decode('utf8', 'replace'))
+        return user["email"], state
 
 class LocalOkpyOAuthenticator(LocalAuthenticator, OkpyOAuthenticator):
     """A version that mixes in local system user creation"""
