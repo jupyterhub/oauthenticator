9e34afcb6f8f3bf2d2d26609b9687ed3b9617d45
Rafael Ladislau Alves <rafael.ladislau@nyu.edu>
2017-05-16 17:48:25 -0400

    Updating Generic acording to OpenID Connect specifications

--- oauthenticator/generic.py
+++ oauthenticator/generic.py
@@ -6,6 +6,7 @@ Custom Authenticator to use generic OAuth2 with JupyterHub
 import json
 import os
 import base64
+import urllib
 
 from tornado.auth import OAuth2Mixin
 from tornado import gen, web
@@ -76,7 +77,7 @@ class GenericOAuthenticator(OAuthenticator):
             grant_type='authorization_code'
         )
 
-        url = url_concat(self.token_url, params)
+        url = self.token_url
 
         b64key = base64.b64encode(
             bytes(
@@ -93,7 +94,7 @@ class GenericOAuthenticator(OAuthenticator):
         req = HTTPRequest(url,
                           method="POST",
                           headers=headers,
-                          body=''  # Body is required for a POST...
+                          body=urllib.parse.urlencode(params)  # Body is required for a POST...
                           )
 
         resp = yield http_client.fetch(req)
