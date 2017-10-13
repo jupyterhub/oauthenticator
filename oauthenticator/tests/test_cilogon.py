e227ee7ee5913cb426ffddd626babf905061a96e
Min RK <benjaminrk@gmail.com>
2017-10-13 12:12:31 +0200

    update cilogon test

--- oauthenticator/tests/test_cilogon.py
+++ oauthenticator/tests/test_cilogon.py
@@ -1,12 +1,6 @@
-import re
-import functools
 import json
-from io import BytesIO
 
 from pytest import fixture, mark
-from urllib.parse import urlparse, parse_qs
-from tornado.httpclient import HTTPRequest, HTTPResponse
-from tornado.httputil import HTTPHeaders
 
 from ..cilogon import CILogonOAuthenticator
 
@@ -16,7 +10,7 @@ from .mocks import setup_oauth_mock
 def user_model(username):
     """Return a user model"""
     return {
-        'sub': 'http://cilogon.org/dinosaurs/users/%s/1729' % username,
+        'eppn': username + '@serenity.space',
     }
 
 
@@ -38,12 +32,10 @@ def test_cilogon(cilogon_client):
     user_info = yield authenticator.authenticate(handler)
     print(json.dumps(user_info, sort_keys=True, indent=4))
     name = user_info['name']
-    assert name == 'http://cilogon.org/dinosaurs/users/wash/1729'
+    assert name == 'wash@serenity.space'
     auth_state = user_info['auth_state']
     assert 'access_token' in auth_state
     assert auth_state == {
         'access_token': auth_state['access_token'],
-        'cilogon_user': {
-            'sub': name
-        }
+        'cilogon_user': user_model('wash'),
     }
