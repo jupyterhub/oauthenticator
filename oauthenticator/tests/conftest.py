8d34496986e68de8aa1a691a494da08f523cb034
Tim Head <betatim@gmail.com>
2018-02-07 10:57:21 +0100

    Add ioloop fixture that works with tornado 5

--- oauthenticator/tests/conftest.py
+++ oauthenticator/tests/conftest.py
@@ -1,9 +1,25 @@
 """Py.Test fixtures"""
 
 from tornado.httpclient import AsyncHTTPClient
+from tornado import ioloop
 from pytest import fixture
 
 from .mocks import MockAsyncHTTPClient
+
+
+@fixture
+def io_loop(request):
+    """Same as pytest-tornado.io_loop, adapted for tornado 5"""
+    io_loop = ioloop.IOLoop()
+    io_loop.make_current()
+
+    def _close():
+        io_loop.clear_current()
+        io_loop.close(all_fds=True)
+
+    request.addfinalizer(_close)
+    return io_loop
+
 
 @fixture
 def client(io_loop, request):
