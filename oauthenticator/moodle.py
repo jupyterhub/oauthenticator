464c4aa73476b3bf4a39369645f615ffe3204ab0
Christopher Hench <chris.l.hench@gmail.com>
2018-03-08 17:35:59 -0800

    clean up userdata params and naming

--- oauthenticator/moodle.py
+++ oauthenticator/moodle.py
@@ -1,5 +1,7 @@
 """
-Custom Authenticator to use generic OAuth2 with JupyterHub
+Custom Authenticator for Moodle to use OAuth2 with JupyterHub.
+Configured for use with Moodle OAuth2 Server Plugin from
+https://github.com/projectestac/moodle-local_oauth .
 """
 
 
@@ -21,33 +23,28 @@ from traitlets import Unicode, Dict
 from .oauth2 import OAuthLoginHandler, OAuthenticator
 
 
-class GenericEnvMixin(OAuth2Mixin):
+class MoodleEnvMixin(OAuth2Mixin):
     _OAUTH_ACCESS_TOKEN_URL = os.environ.get('OAUTH2_TOKEN_URL', '')
     _OAUTH_AUTHORIZE_URL = os.environ.get('OAUTH2_AUTHORIZE_URL', '')
 
 
-class GenericLoginHandler(OAuthLoginHandler, GenericEnvMixin):
+class MoodleLoginHandler(OAuthLoginHandler, MoodleEnvMixin):
     pass
 
 
-class GenericOAuthenticator(OAuthenticator):
+class MoodleOAuthenticator(OAuthenticator):
 
     login_service = Unicode(
-        "SAGE Campus",
+        "Moodle",
         config=True
     )
 
-    login_handler = GenericLoginHandler
+    login_handler = MoodleLoginHandler
 
     userdata_url = Unicode(
         os.environ.get('OAUTH2_USERDATA_URL', ''),
         config=True,
         help="Userdata url to get user data login information"
-    )
-    token_url = Unicode(
-        os.environ.get('OAUTH2_TOKEN_URL', ''),
-        config=True,
-        help="Access token endpoint URL"
     )
 
     username_key = Unicode(
@@ -55,15 +52,11 @@ class GenericOAuthenticator(OAuthenticator):
         config=True,
         help="Userdata username key from returned json for USERDATA_URL"
     )
-    userdata_params = Dict(
-        os.environ.get('OAUTH2_USERDATA_PARAMS', {}),
-        help="Userdata params to get user data login information"
-    ).tag(config=True)
 
-    userdata_method = Unicode(
-        os.environ.get('OAUTH2_USERDATA_METHOD', 'POST'),
+    token_url = Unicode(
+        os.environ.get('OAUTH2_TOKEN_URL', ''),
         config=True,
-        help="Userdata method to get user data login information"
+        help="Access token endpoint URL"
     )
 
     @gen.coroutine
@@ -116,14 +109,10 @@ class GenericOAuthenticator(OAuthenticator):
             "Authorization": "{} {}".format(token_type, access_token)
         }
 
-        url = url_concat(self.userdata_url, self.userdata_params)
-
-        userdata_params = dict(access_token=access_token)
-
-        req = HTTPRequest(url,
-                          method=self.userdata_method,
+        req = HTTPRequest(self.userdata_url,
+                          method="POST",
                           headers=headers,
-                          body=urllib.parse.urlencode(userdata_params)
+                          body=urllib.parse.urlencode({'access_token': access_token})
                           )
         resp = yield http_client.fetch(req)
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
@@ -144,7 +133,7 @@ class GenericOAuthenticator(OAuthenticator):
         }
 
 
-class LocalGenericOAuthenticator(LocalAuthenticator, GenericOAuthenticator):
+class LocalMoodleOAuthenticator(LocalAuthenticator, MoodleOAuthenticator):
 
     """A version that mixes in local system user creation"""
     pass
