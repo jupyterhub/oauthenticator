84ab3cce8db8c599ebd3bbbd836724bea6eb93a1
Min RK <benjaminrk@gmail.com>
2018-08-11 14:46:42 +0200

    release 0.8.0

--- oauthenticator/_version.py
+++ oauthenticator/_version.py
@@ -7,7 +7,7 @@ version_info = (
     0,
     8,
     0,
-    'dev',  # comment-out this line for a release
+    # 'dev',  # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
