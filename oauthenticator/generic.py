a46a349d3e248563a2831b0b75b159e33c5a4c94
root <root@cuv-jupytern1.restonuat.local>
2018-10-10 16:49:39 -0400

    identation fix

--- oauthenticator/generic.py
+++ oauthenticator/generic.py
@@ -120,7 +120,7 @@ class GenericOAuthenticator(OAuthenticator):
         access_token = resp_json['access_token']
         refresh_token = resp_json.get('refresh_token', None)
         token_type = resp_json['token_type']
-	scope = resp_json.get('scope', '')
+        scope = resp_json.get('scope', '')
         if (isinstance(scope, basestring)):
                 scope = (resp_json.get('scope', '')).split(' ')        
 
