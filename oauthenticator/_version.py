e9f71109c29fb290347ddb84cd6b61ee99ff30de
Erik Sundell <erik.i.sundell@gmail.com>
2019-11-27 15:57:50 +0100

    release 0.10.0

--- oauthenticator/_version.py
+++ oauthenticator/_version.py
@@ -7,7 +7,7 @@ version_info = (
     0,
     10,
     0,
-    'dev',  # comment-out this line for a release
+    # 'dev',  # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
