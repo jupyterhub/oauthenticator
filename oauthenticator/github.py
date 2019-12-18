443d39a484875067f9c9d021af99bbb03176fa41
Min RK <benjaminrk@gmail.com>
2019-12-18 10:49:07 +0100

    adopt mixin-less setup in github

--- oauthenticator/github.py
+++ oauthenticator/github.py
@@ -11,6 +11,7 @@ import json
 import os
 import re
 import string
+import warnings
 
 from tornado.auth import OAuth2Mixin
 from tornado import web
@@ -20,39 +21,18 @@ from tornado.httpclient import HTTPRequest, AsyncHTTPClient, HTTPError
 
 from jupyterhub.auth import LocalAuthenticator
 
-from traitlets import List, Set, Unicode
+from traitlets import List, Set, Unicode, default
 
 from .common import next_page_from_links
 from .oauth2 import OAuthLoginHandler, OAuthenticator
 
-# Support github.com and github enterprise installations
-GITHUB_HOST = os.environ.get('GITHUB_HOST') or 'github.com'
-if GITHUB_HOST == 'github.com':
-    GITHUB_API = 'api.github.com'
-else:
-    GITHUB_API = '%s/api/v3' % GITHUB_HOST
-
-# Support github enterprise installations with both http and https
-GITHUB_HTTP = os.environ.get('GITHUB_HTTP')
-if GITHUB_HTTP:
-    GITHUB_PROTOCOL = 'http'
-else:
-    GITHUB_PROTOCOL = 'https'
 
 def _api_headers(access_token):
-    return {"Accept": "application/json",
-            "User-Agent": "JupyterHub",
-            "Authorization": "token {}".format(access_token)
-            }
-
-
-class GitHubMixin(OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = "%s://%s/login/oauth/authorize" % (GITHUB_PROTOCOL, GITHUB_HOST)
-    _OAUTH_ACCESS_TOKEN_URL = "%s://%s/login/oauth/access_token" % (GITHUB_PROTOCOL, GITHUB_HOST)
-
-
-class GitHubLoginHandler(OAuthLoginHandler, GitHubMixin):
-    pass
+    return {
+        "Accept": "application/json",
+        "User-Agent": "JupyterHub",
+        "Authorization": "token {}".format(access_token),
+    }
 
 
 class GitHubOAuthenticator(OAuthenticator):
@@ -63,12 +43,66 @@ class GitHubOAuthenticator(OAuthenticator):
 
     login_service = "GitHub"
 
+    github_url = Unicode("https://github.com", config=True)
+
+    @default("github_url")
+    def _github_url_default(self):
+        github_url = os.environ.get("GITHUB_URL")
+        if not github_url:
+            # fallback on older GITHUB_HOST config,
+            # treated the same as GITHUB_URL
+            host = os.environ.get("GITHUB_HOST")
+            if host:
+                if os.environ.get("GITHUB_HTTP"):
+                    protocol = "http"
+                    warnings.warn(
+                        'Use of GITHUB_HOST with GITHUB_HTTP might be deprecated in the future. '
+                        'Use GITHUB_URL=http://{} to set host and protocol together.'.format(
+                            host
+                        ),
+                        PendingDeprecationWarning,
+                    )
+                else:
+                    protocol = "https"
+                github_url = "{}://{}".format(protocol, host)
+
+        if github_url:
+            if '://' not in github_url:
+                # ensure protocol is included, assume https if missing
+                github_url = 'https://' + github_url
+
+            return github_url
+        else:
+            # nothing specified, this is the true default
+            github_url = "https://github.com"
+
+        # ensure no trailing slash
+        return github_url.rstrip("/")
+
+    github_api = Unicode("https://api.github.com", config=True)
+
+    @default("github_api")
+    def _github_api_default(self):
+        if self.github_url == "https://github.com":
+            return "https://api.github.com"
+        else:
+            return self.github_url + "/api/v3"
+
+    @default("authorize_url")
+    def _authorize_url_default(self):
+        return "%s/login/oauth/authorize" % (self.github_url)
+
+    @default("access_token_url")
+    def _access_token_url_default(self):
+        return "%s/login/oauth/access_token" % (self.github_url)
+
     # deprecated names
     github_client_id = Unicode(config=True, help="DEPRECATED")
 
     def _github_client_id_changed(self, name, old, new):
         self.log.warning("github_client_id is deprecated, use client_id")
         self.client_id = new
+
     github_client_secret = Unicode(config=True, help="DEPRECATED")
 
     def _github_client_secret_changed(self, name, old, new):
@@ -77,11 +111,9 @@ class GitHubOAuthenticator(OAuthenticator):
 
     client_id_env = 'GITHUB_CLIENT_ID'
     client_secret_env = 'GITHUB_CLIENT_SECRET'
-    login_handler = GitHubLoginHandler
 
     github_organization_whitelist = Set(
-        config=True,
-        help="Automatically whitelist members of selected organizations",
+        config=True, help="Automatically whitelist members of selected organizations"
     )
 
     async def authenticate(self, handler, data=None):
@@ -98,19 +130,17 @@ class GitHubOAuthenticator(OAuthenticator):
 
         # GitHub specifies a POST request yet requires URL parameters
         params = dict(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            code=code
+            client_id=self.client_id, client_secret=self.client_secret, code=code
         )
 
-        url = url_concat("%s://%s/login/oauth/access_token" % (GITHUB_PROTOCOL, GITHUB_HOST),
-                         params)
+        url = url_concat(self.access_token_url, params)
 
-        req = HTTPRequest(url,
-                          method="POST",
-                          headers={"Accept": "application/json"},
-                          body=''  # Body is required for a POST...
-                          )
+        req = HTTPRequest(
+            url,
+            method="POST",
+            headers={"Accept": "application/json"},
+            body='',  # Body is required for a POST...
+        )
 
         resp = await http_client.fetch(req)
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
@@ -118,18 +148,19 @@ class GitHubOAuthenticator(OAuthenticator):
         if 'access_token' in resp_json:
             access_token = resp_json['access_token']
         elif 'error_description' in resp_json:
-            raise HTTPError(403,
+            raise HTTPError(
+                403,
                 "An access token was not returned: {}".format(
-                    resp_json['error_description']))
+                    resp_json['error_description']
+                ),
+            )
         else:
-            raise HTTPError(500,
-                "Bad response: %s".format(resp))
+            raise HTTPError(500, "Bad response: %s".format(resp))
 
         # Determine who the logged in user is
-        req = HTTPRequest("%s://%s/user" % (GITHUB_PROTOCOL, GITHUB_API),
-                          method="GET",
-                          headers=_api_headers(access_token)
-                          )
+        req = HTTPRequest(
+            self.github_api + "/user", method="GET", headers=_api_headers(access_token)
+        )
         resp = await http_client.fetch(req)
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
 
@@ -141,7 +172,9 @@ class GitHubOAuthenticator(OAuthenticator):
         # This check is performed here, as it requires `access_token`.
         if self.github_organization_whitelist:
             for org in self.github_organization_whitelist:
-                user_in_org = await self._check_organization_whitelist(org, username, access_token)
+                user_in_org = await self._check_organization_whitelist(
+                    org, username, access_token
+                )
                 if user_in_org:
                     break
             else:  # User not found in member list for any organisation
@@ -171,9 +204,15 @@ class GitHubOAuthenticator(OAuthenticator):
         # With empty scope (even if authenticated by an org member), this
         #  will only await public org members.  You want 'read:org' in order
         #  to be able to iterate through all members.
-        check_membership_url = "%s://%s/orgs/%s/members/%s" % (GITHUB_PROTOCOL, GITHUB_API, org, username)
+        check_membership_url = "%s/orgs/%s/members/%s" % (
+            self.github_api,
+            org,
+            username,
+        )
         req = HTTPRequest(check_membership_url, method="GET", headers=headers)
-        self.log.debug("Checking GitHub organization membership: %s in %s?", username, org)
+        self.log.debug(
+            "Checking GitHub organization membership: %s in %s?", username, org
+        )
         resp = await http_client.fetch(req, raise_error=False)
         print(resp)
         if resp.code == 204:
@@ -198,4 +237,5 @@ class GitHubOAuthenticator(OAuthenticator):
 class LocalGitHubOAuthenticator(LocalAuthenticator, GitHubOAuthenticator):
 
     """A version that mixes in local system user creation"""
+
     pass
