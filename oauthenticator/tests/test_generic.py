5792d9932cb31c31af49022cf02edc3038069f59
Manuel Martins <Manuel.Martins@ecmwf.int>
2020-12-04 18:45:31 +0000

    added allowed_groups and admin_groups to generic.py added documentation and tests

--- oauthenticator/tests/test_generic.py
+++ oauthenticator/tests/test_generic.py
@@ -1,4 +1,4 @@
-from pytest import fixture, mark
+from pytest import fixture
 
 from ..generic import GenericOAuthenticator
 
@@ -15,16 +15,23 @@ def user_model(username, **kwargs):
     user.update(kwargs)
     return user
 
-def Authenticator(**kwargs):
+
+def get_authenticator(**kwargs):
     return GenericOAuthenticator(
         token_url='https://generic.horse/oauth/access_token',
         userdata_url='https://generic.horse/oauth/userinfo',
         **kwargs
     )
 
+
+def get_simple_handler(generic_client):
+    return generic_client.handler_for_user(user_model('wash'))
+
+
 @fixture
 def generic_client(client):
-    setup_oauth_mock(client,
+    setup_oauth_mock(
+        client,
         host='generic.horse',
         access_token_path='/oauth/access_token',
         user_path='/oauth/userinfo',
@@ -35,9 +42,9 @@ def generic_client(client):
 async def test_generic(generic_client):
     with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
         fake_client.return_value = generic_client
-        authenticator = Authenticator()
+        authenticator = get_authenticator()
 
-        handler = generic_client.handler_for_user(user_model('wash'))
+        handler = get_simple_handler(generic_client)
         user_info = await authenticator.authenticate(handler)
         assert sorted(user_info) == ['auth_state', 'name']
         name = user_info['name']
@@ -52,7 +59,7 @@ async def test_generic(generic_client):
 async def test_generic_callable_username_key(generic_client):
     with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
         fake_client.return_value = generic_client
-        authenticator = Authenticator(
+        authenticator = get_authenticator(
             username_key=lambda r: r['alternate_username']
         )
         handler = generic_client.handler_for_user(
@@ -60,3 +67,100 @@ async def test_generic_callable_username_key(generic_client):
         )
         user_info = await authenticator.authenticate(handler)
         assert user_info['name'] == 'zoe'
+
+
+async def test_generic_callable_groups_claim_key_with_allowed_groups(generic_client):
+    with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
+        fake_client.return_value = generic_client
+        authenticator = get_authenticator(
+            scope=['openid', 'profile', 'roles'],
+            claim_groups_key=lambda r: r.get('policies').get('roles'),
+            allowed_groups=['super_user']
+        )
+        handler = generic_client.handler_for_user(
+            user_model('wash', alternate_username='zoe', policies={'roles': ['super_user']})
+        )
+        user_info = await authenticator.authenticate(handler)
+        assert user_info['name'] == 'wash'
+
+
+async def test_generic_groups_claim_key_with_allowed_groups(generic_client):
+    with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
+        fake_client.return_value = generic_client
+        authenticator = get_authenticator(
+            scope=['openid', 'profile', 'roles'],
+            claim_groups_key='groups',
+            allowed_groups=['super_user']
+        )
+        handler = generic_client.handler_for_user(
+            user_model('wash', alternate_username='zoe', groups=['super_user'])
+        )
+        user_info = await authenticator.authenticate(handler)
+        assert user_info['name'] == 'wash'
+
+
+async def test_generic_groups_claim_key_with_allowed_groups_unauthorized(generic_client):
+    with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
+        fake_client.return_value = generic_client
+        authenticator = get_authenticator(
+            scope=['openid', 'profile', 'roles'],
+            claim_groups_key='groups',
+            allowed_groups=['user']
+        )
+        handler = generic_client.handler_for_user(
+            user_model('wash', alternate_username='zoe', groups=['public'])
+        )
+        user_info = await authenticator.authenticate(handler)
+        assert user_info is None
+
+
+async def test_generic_groups_claim_key_with_allowed_groups_and_admin_groups(generic_client):
+    with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
+        fake_client.return_value = generic_client
+        authenticator = get_authenticator(
+            scope=['openid', 'profile', 'roles'],
+            claim_groups_key='groups',
+            allowed_groups=['user'],
+            admin_groups=['administrator'],
+        )
+        handler = generic_client.handler_for_user(
+            user_model('wash', alternate_username='zoe', groups=['user', 'administrator'])
+        )
+        user_info = await authenticator.authenticate(handler)
+        assert user_info['name'] == 'wash'
+        assert user_info['admin'] is True
+
+
+async def test_generic_groups_claim_key_with_allowed_groups_and_admin_groups_not_admin(generic_client):
+    with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
+        fake_client.return_value = generic_client
+        authenticator = get_authenticator(
+            scope=['openid', 'profile', 'roles'],
+            claim_groups_key='groups',
+            allowed_groups=['user'],
+            admin_groups=['administrator'],
+        )
+        handler = generic_client.handler_for_user(
+            user_model('wash', alternate_username='zoe', groups=['user'])
+        )
+        user_info = await authenticator.authenticate(handler)
+        assert user_info['name'] == 'wash'
+        assert user_info['admin'] is False
+
+
+async def test_generic_callable_groups_claim_key_with_allowed_groups_and_admin_groups(generic_client):
+    with mock.patch.object(GenericOAuthenticator, 'http_client') as fake_client:
+        fake_client.return_value = generic_client
+        authenticator = get_authenticator(
+            username_key=lambda r: r['alternate_username'],
+            scope=['openid', 'profile', 'roles'],
+            claim_groups_key=lambda r: r.get('policies').get('roles'),
+            allowed_groups=['user', 'public'],
+            admin_groups=['administrator'],
+        )
+        handler = generic_client.handler_for_user(
+            user_model('wash', alternate_username='zoe', policies={'roles': ['user', 'administrator']})
+        )
+        user_info = await authenticator.authenticate(handler)
+        assert user_info['name'] == 'zoe'
+        assert user_info['admin'] is True
