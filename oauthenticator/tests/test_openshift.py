09b93664febcc73c1a9c7e69e3fcb74577af06f9
Will Eaton <me@wseaton.com>
2021-02-09 10:10:36 -0500

    add tests for new cases

--- oauthenticator/tests/test_openshift.py
+++ oauthenticator/tests/test_openshift.py
@@ -39,7 +39,7 @@ async def test_openshift(openshift_client):
 
 async def test_openshift_allowed_groups(openshift_client):
     authenticator = OpenShiftOAuthenticator()
-    authenticator.allowed_groups = ['group1']
+    authenticator.allowed_groups = {'group1'}
     authenticator.openshift_auth_api_url = "https://openshift.default.svc.cluster.local"
     handler = openshift_client.handler_for_user(user_model('wash'))
     user_info = await authenticator.authenticate(handler)
@@ -55,8 +55,41 @@ async def test_openshift_allowed_groups(openshift_client):
 
 async def test_openshift_not_in_allowed_groups(openshift_client):
     authenticator = OpenShiftOAuthenticator()
-    authenticator.allowed_groups = ['group3']
+    authenticator.allowed_groups = {'group3'}
     authenticator.openshift_auth_api_url = "https://openshift.default.svc.cluster.local"
     handler = openshift_client.handler_for_user(user_model('wash'))
     user_info = await authenticator.authenticate(handler)
     assert user_info == None
+
+
+async def test_openshift_not_in_allowed_groups_but_is_admin(openshift_client):
+    authenticator = OpenShiftOAuthenticator()
+    authenticator.allowed_groups = {'group3'}
+    authenticator.admin_groups = {'group1'}
+    authenticator.openshift_auth_api_url = "https://openshift.default.svc.cluster.local"
+    handler = openshift_client.handler_for_user(user_model('wash'))
+    user_info = await authenticator.authenticate(handler)
+    assert sorted(user_info) == ['admin', 'auth_state', 'name']
+    assert user_info['admin'] == True
+
+
+async def test_openshift_in_allowed_groups_and_is_admin(openshift_client):
+    authenticator = OpenShiftOAuthenticator()
+    authenticator.allowed_groups = {'group2'}
+    authenticator.admin_groups = {'group1'}
+    authenticator.openshift_auth_api_url = "https://openshift.default.svc.cluster.local"
+    handler = openshift_client.handler_for_user(user_model('wash'))
+    user_info = await authenticator.authenticate(handler)
+    assert sorted(user_info) == ['admin', 'auth_state', 'name']
+    assert user_info['admin'] == True
+
+
+async def test_openshift_in_allowed_groups_and_is_not_admin(openshift_client):
+    authenticator = OpenShiftOAuthenticator()
+    authenticator.allowed_groups = {'group2'}
+    authenticator.admin_groups = {'group3'}
+    authenticator.openshift_auth_api_url = "https://openshift.default.svc.cluster.local"
+    handler = openshift_client.handler_for_user(user_model('wash'))
+    user_info = await authenticator.authenticate(handler)
+    assert sorted(user_info) == ['admin', 'auth_state', 'name']
+    assert user_info['admin'] == False
