d8ea0b6f11dbc3dac35aada9d17aef6ccd2da6a4
Erik Sundell <erik.i.sundell@gmail.com>
2021-07-19 14:32:09 +0200

    back to dev

--- oauthenticator/_version.py
+++ oauthenticator/_version.py
@@ -5,8 +5,8 @@
 version_info = (
     14,
     1,
-    0,
-    # 'dev',  # comment-out this line for a release
+    1,
+    'dev',  # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
