c5188fc90b5edf2ad0805677cac0f9d1262b5861
Erik Sundell <erik.i.sundell@gmail.com>
2021-07-19 14:31:47 +0200

    release 14.1.0

--- oauthenticator/_version.py
+++ oauthenticator/_version.py
@@ -4,9 +4,9 @@
 
 version_info = (
     14,
-    0,
     1,
-    'dev',  # comment-out this line for a release
+    0,
+    # 'dev',  # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
