2dc5636bd0240bd62d2b90570be99e9b7814da6b
Erik Sundell <erik.i.sundell@gmail.com>
2022-11-28 08:28:22 +0100

    globus: update outdated link

--- oauthenticator/globus.py
+++ oauthenticator/globus.py
@@ -304,7 +304,7 @@ class GlobusOAuthenticator(OAuthenticator):
             raise HTTPError(
                 403,
                 f"This site is restricted to {self.identity_provider} accounts. "
-                "Please link your account at globus.org/app/account.",
+                "Please link your account at app.globus.org/account.",
             )
         return username
 
